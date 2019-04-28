ecc93537c20eb1ab04b6f185a0dbbcf84acc198e
zuokunpeng <zuokunpengccsu@gmail.com>
2019-04-28 16:39:01 +0800

    简单工厂模式

---
+++ SimpleFactory/abstract_factory.h
@@ -0,0 +1,29 @@
+/*
+ * abstract_factory.h
+ *
+ *  Created on: 2019年4月28日
+ *      Author: Zuokp
+ */
+
+#ifndef ABSTRACT_FACTORY_H_
+#define ABSTRACT_FACTORY_H_
+#include "abstract_product.h"
+
+class AbstractFactory
+{
+public:
+	AbstractFactory();
+	virtual ~AbstractFactory();
+
+	virtual AbstractProduct* createProduct(const char type) = 0;
+};
+
+class SimpleFactory : public AbstractFactory
+{
+public:
+	SimpleFactory();
+	virtual ~SimpleFactory();
+
+	AbstractProduct* createProduct(const char type);
+};
+#endif /* ABSTRACT_FACTORY_H_ */
